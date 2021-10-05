1e233b66222bdc3d661243bb44326d5424179e90
Mosoriol <osoriomig@gmail.com>
2021-10-04 22:10:17 -0600

    inicial
    commit inicial

---
+++ cms/resources/views/modulos/menu.blade.php
@@ -0,0 +1,71 @@
+<!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">Administracion</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+     
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+        
+              <li class="nav-item">
+                <a href="./index.html" class="nav-link">
+                 <i class="fas fa-home"></i>
+                  <p>Inicio</p>
+                </a>
+              </li>
+              
+
+              <li class="nav-item">
+                <a href="./index.html" class="nav-link">
+                  <i class="fas fa-users"></i>
+                  <p>Usuarios</p>
+                </a>
+              </li>
+
+
+              <li class="nav-item">
+                <a href="./index.html" class="nav-link">
+                  <i class="fas fa-tools"></i>
+                  <p>Proyectos</p>
+                </a>
+              </li>
+
+
+              <li class="nav-item">
+                <a href="./index.html" class="nav-link">
+                  <i class="fas fa-book-open"></i>
+                  <p>Biografias</p>
+                </a>
+              </li>
+
+
+              <li class="nav-item">
+                <a href="./index.html" class="nav-link">
+                  <i class="fab fa-slideshare"></i>
+                  <p>Slider</p>
+                </a>
+              </li>
+
+
+              <li class="nav-item">
+                <a href="./index.html" class="nav-link">
+                  <i class="far fa-comment"></i>
+                  <p>Mensajes</p>
+                </a>
+              </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
